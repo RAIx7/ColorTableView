0ca7526427de7782a443f64f85992972b9c0e04e
Sanjay Rai <rai7@live.co.uk>
2017-03-21 20:00:29 +0000

    When clicking table view cell, new screen is presented

--- ColorsTableView/ViewController.swift
+++ ColorsTableView/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let colors = ["Blue", "Yellow"]
-    let otherColors = ["Red", "Green"]
+    let colors = ["Blue", "Yellow", "Red", "Green"]
+    var a = "A"
+    var b = "B"
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         
     }
 
@@ -25,55 +29,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        if section == 0 {
-            return "Blue and Yellow"
-        }
-        else {
-            return "Red and Green"
-        }
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-        
-    }
-    
-    
-
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if section == 0 {
             return colors.count
-        } else {
-            return otherColors.count
-        }
+    
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ColorTableViewCell
        
-        if indexPath.section == 0 {
             cell.colorImageView.image = UIImage(named:colors[indexPath.row] + ".jpg")
             cell.label.text = colors[indexPath.row]
             return cell
-            
-        } else {
-            
-            cell.colorImageView.image = UIImage(named:otherColors[indexPath.row] + ".jpg")
-            cell.label.text = otherColors[indexPath.row]
-            return cell
-            
-        }
-
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        if indexPath.row == 0 {
+            let viewController = storyboard?.instantiateViewController(withIdentifier: a)
+            self.navigationController?.pushViewController(viewController!, animated: true)
+            print(indexPath.row)
+            print("Mission early complete")
+        } else {
+            let viewController = storyboard?.instantiateViewController(withIdentifier: b)
+            self.navigationController?.pushViewController(viewController!, animated: true)
+            print(indexPath.row)
+            print("Mission Accomplished, good work")
+        }
 
-    
-    
-    
+
+        
+    }
 }
 
